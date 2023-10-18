b9efac56126f4937f9302194d13250893eee2da4
Mohamed <s2000588@edu.bc.fi>
2023-10-18 23:09:46 +0300

    create navbar

---
+++ src/components/data/NavbarData/Index.ts
@@ -0,0 +1,26 @@
+ import { NavLinkInterface } from "../../interfaces";
+
+export const NavBarLinks: NavLinkInterface[] = [
+  {
+    to: "/",
+    text: "Home",
+    id: 1,
+  },
+  {
+    to: "/about",
+    text: "About",
+    id: 2,
+  },
+  {
+    to: "/projects",
+    text: "Projects",
+    id: 3,
+  },
+  {
+    to: "/contact",
+    text: "Contact",
+    id: 4,
+  },
+];
+
+ 
