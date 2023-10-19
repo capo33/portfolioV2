5440d2b95ddfd56deaf5d9e0d1b21d9f5945426c
Mohamed <s2000588@edu.bc.fi>
2023-10-19 12:34:17 +0300

    Split nav svg into another component

---
+++ src/assets/svg/NavSVG.tsx
@@ -0,0 +1,66 @@
+const HomeSVG = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-5 h-5 mr-1'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
+    />
+  </svg>
+);
+const ProjectsSVG = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-5 h-5 mr-1'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z'
+    />
+  </svg>
+);
+const AboutSVG = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-5 h-5 mr-1'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'
+    />
+  </svg>
+);
+const ContactSVG = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-5 h-5 mr-1'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z'
+    />
+  </svg>
+);
+
+export { HomeSVG, ProjectsSVG, AboutSVG, ContactSVG };
