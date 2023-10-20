3b1fe8f3e04bb667f4ec4ce224c8fdee83272097
Mohamed <s2000588@edu.bc.fi>
2023-10-20 21:15:41 +0300

    change home imnage

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,7 +3,7 @@ import { GitHubSVG } from "../../assets/svg/NavSVG";
 
 const Home = () => {
   return (
-    <section className='bg-  dark:bg-gray-900'>
+    <section className='bg-    dark:bg-gray-900'>
       <div className='grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12  '>
         <div className='mr-auto place-self-center lg:col-span-7'>
           <h1 className='max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white'>
