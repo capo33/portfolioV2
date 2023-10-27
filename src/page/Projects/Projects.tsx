1a49f7e056be28555712ec28c23ba28e2777e61d
Mohamed <s2000588@edu.bc.fi>
2023-10-27 08:43:57 +0300

    add globalsvg icon

--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { portfolioProjects } from "./test";
 import { motion, AnimatePresence } from "framer-motion";
-import { GitHubSVG } from "../../assets/svg/NavSVG";
+import { GitHubSVG, GlobalSVG } from "../../assets/svg/NavSVG";
 
 const Projects = () => {
   const [activeTab, setActiveTab] = useState<string>("All");
@@ -107,20 +107,7 @@ const Projects = () => {
                     <div className='mt-1 p-2'>
                       <div className='mt-3 flex justify-between'>
                         <div className='flex items-center space-x-1.5 rounded bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
-                          <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            strokeWidth={1.5}
-                            stroke='currentColor'
-                            className='h-4 w-4'
-                          >
-                            <path
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
-                            />
-                          </svg>
+                          {GlobalSVG("h-4 w-4")}
 
                           <button className='text-sm'>Live Demo</button>
                         </div>
