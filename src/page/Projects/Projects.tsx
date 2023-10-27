e72beee040be94c6b0272fcd597dbb00a9007e90
Mohamed <s2000588@edu.bc.fi>
2023-10-27 08:20:14 +0300

    update project card size

--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -29,7 +29,6 @@ const Projects = () => {
             All Projects
           </h1>
         </div>
-       
       </div>
 
       <div className='flex flex-wrap items-center  overflow-x-auto overflow-y-hidden py-10 justify-center text-gray-800'>
@@ -90,49 +89,47 @@ const Projects = () => {
                 console.log(item);
 
                 return (
-                  <article className='rounded bg-white p-3 shadow-lg hover:shadow-xl'>
-                    <a href='#'>
-                      <div className='relative flex items-end overflow-hidden rounded'>
-                        <img
-                          src={
-                            "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH"
-                          }
-                          alt='Photo'
-                        />
-                      </div>
+                  <article className='rounded p-3 shadow-lg hover:shadow-xl flex flex-col justify-around'>
+                    <div className='relative flex-grow items-end overflow-hidden rounded'>
+                      <img
+                        src={
+                          "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH"
+                        }
+                        alt='Photo'
+                      />
                       <div className='mt-1 p-2'>
-                        <h2 className='text-slate-700 flex-grow'>
-                          {item.title}
-                        </h2>
-                        <p className='mt-1 text-sm text-slate-400 flex-grow'>
+                        <h2 className='text-slate-700'>{item.title}</h2>
+                        <p className='mt-1 text-sm text-slate-400'>
                           {item.description}
                         </p>
-                        <div className='mt-3 flex justify-between flex-grow'>
-                          <div className='flex items-center space-x-1.5 rounded bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
-                            <svg
-                              xmlns='http://www.w3.org/2000/svg'
-                              fill='none'
-                              viewBox='0 0 24 24'
-                              strokeWidth={1.5}
-                              stroke='currentColor'
-                              className='h-4 w-4'
-                            >
-                              <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
-                              />
-                            </svg>
+                      </div>
+                    </div>
+                    <div className='mt-1 p-2'>
+                      <div className='mt-3 flex justify-between flex-grow'>
+                        <div className='flex items-center space-x-1.5 rounded bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth={1.5}
+                            stroke='currentColor'
+                            className='h-4 w-4'
+                          >
+                            <path
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
+                              d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
+                            />
+                          </svg>
 
-                            <button className='text-sm'>Live Demo</button>
-                          </div>
-                          <div className='flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
-                            {GitHubSVG("h-4 w-4")}
-                            <button className='text-sm'>Source Code</button>
-                          </div>
+                          <button className='text-sm'>Live Demo</button>
+                        </div>
+                        <div className='flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
+                          {GitHubSVG("h-4 w-4")}
+                          <button className='text-sm'>Source Code</button>
                         </div>
                       </div>
-                    </a>
+                    </div>
                   </article>
                 );
               }).slice(0, 6)}
