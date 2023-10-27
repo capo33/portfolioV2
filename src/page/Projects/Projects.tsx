7321fae6e89685c03f18358a3210795961e612b1
Mohamed <s2000588@edu.bc.fi>
2023-10-27 08:39:59 +0300

    change card layout

--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -83,14 +83,14 @@ const Projects = () => {
           exit={{ y: -10, opacity: 0 }}
           transition={{ ease: "easeIn", duration: 0.5 }}
         >
-          <section className='py-10 bg-gray-100'>
+          <section className='py-10'>
             <div className='mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
               {Items.map((item) => {
                 console.log(item);
 
                 return (
                   <article className='rounded p-3 shadow-lg hover:shadow-xl flex flex-col justify-around'>
-                    <div className='relative flex-grow items-end overflow-hidden rounded'>
+                    <div className='overflow-hidden rounded'>
                       <img
                         src={
                           "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH"
@@ -105,7 +105,7 @@ const Projects = () => {
                       </div>
                     </div>
                     <div className='mt-1 p-2'>
-                      <div className='mt-3 flex justify-between flex-grow'>
+                      <div className='mt-3 flex justify-between'>
                         <div className='flex items-center space-x-1.5 rounded bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
                           <svg
                             xmlns='http://www.w3.org/2000/svg'
@@ -124,7 +124,7 @@ const Projects = () => {
 
                           <button className='text-sm'>Live Demo</button>
                         </div>
-                        <div className='flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
+                        <div className='flex items-center space-x-1.5 rounded bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600'>
                           {GitHubSVG("h-4 w-4")}
                           <button className='text-sm'>Source Code</button>
                         </div>
@@ -135,34 +135,6 @@ const Projects = () => {
               }).slice(0, 6)}
             </div>
           </section>
-
-          {/* <section className='py-10 bg-gray-100'>
-            <div className='mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
-              {Items.map((item) => {
-                return (
-                  <div className='bg-white shadow-md dark:bg-gray-800'>
-                    <a href='#' className=''>
-                      <img
-                        src='https://i.postimg.cc/904JVBhh/pexels-andrea-piacquadio-840996.jpg'
-                        alt=''
-                        className='object-cover w-full h-64'
-                      />
-                    </a>
-                    <div className='p-5 flex flex-col justify-between'>
-                      <a href='#' className=''>
-                        <h2 className='mb-2 text-xl font-bold tracking-tight text-gray-700 dark:text-gray-400'>
-                          {item.title}
-                        </h2>
-                      </a>
-                      <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-                        {item.description}
-                      </p>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </section> */}
         </motion.div>
       </AnimatePresence>
     </section>
