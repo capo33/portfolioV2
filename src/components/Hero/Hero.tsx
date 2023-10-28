944ee86ecd7fd1ddca7d2c118a0dd83e8acb878a
Mohamed <s2000588@edu.bc.fi>
2023-10-28 18:37:22 +0300

    Add MitionAnimate to hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Section from "../../shared/Section/Section";
+import { MitionAnimate } from "..";
 
 interface InfoSectionProps {
   topLine: string;
@@ -25,6 +26,7 @@ const Hero = ({
   description,
 }: InfoSectionProps) => {
   return (
+    <MitionAnimate>
     <Section
       topLine={topLine}
       firstHeadLine={firstHeadLine}
@@ -32,23 +34,24 @@ const Hero = ({
       description={description}
       img={img}
     >
-      <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
-        <a
-          href='https://github.com/themesberg/landwind'
-          className='inline-flex items-center justify-center  w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-        >
-          {firstIcon}
-          {firstLabel}
-        </a>
-        <a
-          href='https://github.com/themesberg/landwind'
-          className='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-        >
-          {secondIcon}
-          {secondLabel}
-        </a>
-      </div>
+        <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
+          <a
+            href='https://github.com/themesberg/landwind'
+            className='inline-flex items-center justify-center  w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+          >
+            {firstIcon}
+            {firstLabel}
+          </a>
+          <a
+            href='https://github.com/themesberg/landwind'
+            className='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+          >
+            {secondIcon}
+            {secondLabel}
+          </a>
+        </div>
     </Section>
+      </MitionAnimate>
   );
 };
 
