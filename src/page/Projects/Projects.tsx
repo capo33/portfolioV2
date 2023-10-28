79c830afbef0862e0dbc773a5562cf740b3dda2c
Mohamed <s2000588@edu.bc.fi>
2023-10-28 12:46:59 +0300

    delete app.css codes

--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -64,7 +64,7 @@ const Projects = () => {
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           exit={{ y: -10, opacity: 0 }}
-          transition={{ ease: "easeIn", duration: 0.5 }}
+          transition={{ duration: 0.5 }}
         >
           <section className='py-10'>
             <div className='mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
