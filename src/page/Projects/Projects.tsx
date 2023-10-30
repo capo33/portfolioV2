01f899f09b7b5cf857f387ff771fbc9c233d0766
Mohamed <s2000588@edu.bc.fi>
2023-10-30 20:38:33 +0200

    updata tabs data & creatre icons for Tabs data

--- src/page/Projects/Projects.tsx
+++ src/page/Projects/Projects.tsx
@@ -5,12 +5,24 @@ import { Card, MitionAnimate } from "../../components";
 import { portfolioProjects } from "../../data/ProjectData";
 import { GitHubSVG } from "../../assets/svg/NavSVG";
 import Anchor from "../../shared/Anchor/Anchor";
+import {
+  FrontendSVG,
+  ExperimentSVG,
+  StackSVG,
+  TableSVG,
+} from "../../assets/svg/TabsSVG";
 
 const Projects = () => {
   const [activeTab, setActiveTab] = useState<string>("All");
   const [Items, setItems] = React.useState<IProjects[]>(portfolioProjects);
 
-  const tabs = ["All", "Experimental", "Frontend", "Fullstack"];
+  // const tabs = ["All", "Experimental", "Frontend", "Fullstack"];
+  const tabs = [
+    { id: 1, name: "All", icon: TableSVG },
+    { id: 2, name: "Experimental", icon: ExperimentSVG },
+    { id: 3, name: "Frontend", icon: FrontendSVG },
+    { id: 4, name: "Fullstack", icon: StackSVG },
+  ];
 
   const handleFilter = (e: React.MouseEvent<HTMLSpanElement>) => {
     const target = e.target as HTMLSpanElement;
@@ -41,13 +53,13 @@ const Projects = () => {
       </p>
 
       {/* Tabs */}
-      <ul className='flex flex-wrap items-center my-10 text-lg font-medium text-center gap-2 text-gray-500'>
+      <ul className='flex flex-wrap items-center justify-center my-10 text-lg font-medium text-center gap-2 text-gray-500'>
         {tabs.map((tab) => (
           <li>
             <button
-              key={tab}
+              key={tab.id}
               onClick={
-                tab === tabs[0]
+                tab.name === tabs[0].name
                   ? () => {
                       setItems(portfolioProjects);
                       setActiveTab("All");
@@ -55,19 +67,13 @@ const Projects = () => {
                   : handleFilter
               }
               className={`inline-flex items-center justify-center px-4 py-1 rounded hover:bg-gray-200 hover:text-black ${
-                activeTab === tab ? "bg-gray-500 text-white duration-300" : ""
+                activeTab === tab.name
+                  ? "bg-gray-500 text-white duration-300"
+                  : ""
               }`}
             >
-              <svg
-                className='w-4 h-4 mr-2 text-gray-400'
-                aria-hidden='true'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='currentColor'
-                viewBox='0 0 20 20'
-              >
-                <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
-              </svg>
-              {tab}
+              {tab.icon()}
+              {tab.name}
             </button>
           </li>
         ))}
