60c0fd0ad76a39ce2cc660f06fdd34e23c0bfe69
Mohamed <s2000588@edu.bc.fi>
2023-10-30 14:15:11 +0200

    add main tag to app copomonent

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,11 +3,11 @@ import { HeroObjOne, HeroObjTwo } from "../../data/HeroData";
 
 const Home = () => {
   return (
-    <main className='container mx-auto px-4 space-y-12 md:space-y-16 lg:space-y-20 py-10'>
+    <section className='container mx-auto px-4 space-y-12 md:space-y-16 lg:space-y-20 py-10'>
       <Hero {...HeroObjOne} />
       <Marquee link='/contact' />
       <Showcase {...HeroObjTwo} />
-    </main>
+    </section>
   );
 };
 
