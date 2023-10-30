ffa4bed2b318a4c9bf12590ac78f369169289289
Mohamed <s2000588@edu.bc.fi>
2023-10-30 23:07:23 +0200

    remove MitionAnimate from hero & showCase

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { MitionAnimate } from "..";
 import { SharedSection, AnchorTag } from "../../shared";
 
 interface InfoSectionProps {
@@ -27,28 +26,26 @@ const Hero = ({
   description,
 }: InfoSectionProps) => {
   return (
-    <MitionAnimate>
-      <SharedSection
-        topLine={topLine}
-        firstHeadLine={firstHeadLine}
-        secondHeadLine={secondHeadLine}
-        description={description}
-        img={img}
-      >
-        <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
-          <AnchorTag
-            icon={firstIcon}
-            label={firstLabel}
-            aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-          />
-          <AnchorTag
-            icon={secondIcon}
-            label={secondLabel}
-            aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-          />
-        </div>
-      </SharedSection>
-    </MitionAnimate>
+    <SharedSection
+      topLine={topLine}
+      firstHeadLine={firstHeadLine}
+      secondHeadLine={secondHeadLine}
+      description={description}
+      img={img}
+    >
+      <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
+        <AnchorTag
+          icon={firstIcon}
+          label={firstLabel}
+          aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+        />
+        <AnchorTag
+          icon={secondIcon}
+          label={secondLabel}
+          aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
+        />
+      </div>
+    </SharedSection>
   );
 };
 
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,12 +1,14 @@
-import { Hero, Marquee, Showcase } from "../../components";
+import { Hero, Marquee, MitionAnimate, Showcase } from "../../components";
 import { HeroObjOne, HeroObjTwo } from "../../data/HeroData";
 
 const Home = () => {
   return (
     <section className='container mx-auto px-4 space-y-12 md:space-y-16 lg:space-y-20 py-10'>
-      <Hero {...HeroObjOne} />
-      <Marquee link='/contact' />
-      <Showcase {...HeroObjTwo} />
+      <MitionAnimate>
+        <Hero {...HeroObjOne} />
+        <Marquee link='/contact' />
+        <Showcase {...HeroObjTwo} />
+      </MitionAnimate>
     </section>
   );
 };
