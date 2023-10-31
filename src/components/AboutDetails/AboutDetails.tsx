ef51c23ffcd3e241efff3a007bb9dc423f96c99b
Mohamed <s2000588@edu.bc.fi>
2023-10-31 22:28:57 +0200

    update Abput component

--- src/components/AboutDetails/AboutDetails.tsx
+++ src/components/AboutDetails/AboutDetails.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SharedSection } from "../../shared";
-import Capo from "../../assets/images/hero.png";
-
+ 
 interface InfoSectionProps {
   topLine: string;
   firstHeadLine?: string;
@@ -29,13 +28,15 @@ const AboutDetails = ({
   img,
 }: InfoSectionProps) => {
   return (
-    <SharedSection
-      topLine={topLine}
-      firstHeadLine={firstHeadLine}
-      secondHeadLine={secondHeadLine}
-      img={img}
-     >
-      <div className='grid gap-4 z-20'>
+    <>
+      <SharedSection
+        topLine={topLine}
+        firstHeadLine={firstHeadLine}
+        secondHeadLine={secondHeadLine}
+        img={img}
+        imgStart
+      >
+        {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 z-20'>
         {description &&
           description.map((item) => (
             <ol className='shadow-md '>
@@ -54,13 +55,56 @@ const AboutDetails = ({
               </li>
             </ol>
           ))}
-      </div>
-      <div className='flex-1 text-center py-10 lg:py-0'>
-        <img src={img} className='object-cover' />
-      </div>
-    </SharedSection>
-
-
+      </div> */}
+        <div className=' '>
+          <div className='text-gray-500 sm:text-lg dark:text-gray-400'>
+            {/* List */}
+            <ul
+              role='list'
+              className='pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700'
+            >
+              {description &&
+                description.map((item) => (
+                  <li className='flex space-x-3 '>
+                    <svg
+                      className='flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400'
+                      fill='currentColor'
+                      viewBox='0 0 20 20'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                    {/* Icon */}
+                    <span className='lg:text-lg leading-tight text-gray-900 dark:text-white'>
+                      {item.title}
+                    </span>
+                    {item.icon}
+                    {/* {item.x} */}
+                    <span className='text-base leading-tight text-gray-900 dark:text-white'>
+                      {item.title4}
+                    </span>
+                    {item.icon4}
+                    <span className='text-base font-medium leading-tight text-gray-900 dark:text-white'>
+                      {item.title1}
+                    </span>
+                    {item.icon1}
+                    <span className='text-base font-medium leading-tight text-gray-900 dark:text-white'>
+                      {item.title3}
+                    </span>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+        <div className='flex-1 text-center py-10 lg:py-0'>
+          <img src={img} className='object-cover' />
+        </div>
+      </SharedSection>
+    </>
   );
 };
 
--- src/data/AboutData/Index.tsx
+++ src/data/AboutData/Index.tsx
@@ -1,11 +1,10 @@
-
 import py from "./python.svg";
 import dotNet from "./dotnet.svg";
 import aws from "./aws.svg";
 import unity from "./unity.svg";
 import csharp from "./csharp.svg";
 import Capo from "../../assets/images/hero.png";
- 
+
  
 export const AboutObj = {
   imgStart: true,
@@ -16,44 +15,49 @@ export const AboutObj = {
     {
       id: 1,
       title:
-        "👋 Hi, I’m a Frontend Developer with a passion for building beautiful and functional websites and applications.",
+        "I’m a Software Developer with a passion for building beautiful and functional websites and applications.",
     },
-  
+
     {
       id: 3,
-      title: "💻 I studied web-development at Business Collage School.",
+      title:
+        "My coding journey started in 2020 when I decided to change my career path and become a developer.",
     },
     {
       id: 4,
-      title:
-        "👍 Since I started programming, I feel like I have found my passion and I'm always looking for new challenges. I have really enjoyed every moment of this journey.",
+      title: "I studied web-development at Business Collage Helsinki.",
     },
-  
+
     {
       id: 5,
       title:
-        "😉 I use a creative approach to problem solving and I'm always looking for new ways to improve my skills and knowledge.",
+        "During my programming journey, I learned some technologies related to web development, such as JavaScript, React, TypeScript, and NodeJS, etc.. and I really enjoyed every moment of this journey.",
     },
     {
       id: 6,
       title:
-        "🏆 I have experience working as part of a team and individually. I have successfully met deadlines on every project I’ve worked on.",
+        "I motivate myself by looking for new challenges and new ways to improve my skills and knowledge.",
+    },
+    {
+      id: 6,
+      title:
+        "I now have experience working as part of a team and individually. I have successfully met deadlines on every project I’ve worked on.",
     },
     {
       id: 7,
-      title: `"🚀 Currently I'm learning `,
-      icon: <img src={aws} alt="aws" width="25" height="25" />,
+      title: `Currently I'm learning `,
+      icon: <img src={aws} alt='aws' width='25' height='25' />,
       title1: " -",
-      icon1: <img src={py} alt="python" width="25" height="25" />,
+      icon1: <img src={py} alt='python' width='25' height='25' />,
       title4: " -",
-      icon4: <img src={dotNet} alt="dotNet" width="25" height="25" />,
+      icon4: <img src={dotNet} alt='dotNet' width='25' height='25' />,
     },
     {
       id: 8,
-      title: "👨‍👧‍👦 For fun I use ",
-      icon: <img src={csharp} alt="csharp" width="25" height="25" />,
-      title1: " and ",
-      icon1:  <img src={unity} alt="unity" width="25" height="25" />,
+      title: "For fun I use",
+      icon: <img src={csharp} alt='csharp' width='25' height='25' />,
+      title1: "and",
+      icon1: <img src={unity} alt='unity' width='25' height='25' />,
       title3: " to develop games.",
     },
   ],
@@ -61,4 +65,3 @@ export const AboutObj = {
   secondLabel: "something else",
   img: Capo,
 };
-
