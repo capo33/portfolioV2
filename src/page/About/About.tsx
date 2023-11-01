d7b9be0e6a0ef0cb56f32a2b429c9f0f44e6f5b9
Mohamed <s2000588@edu.bc.fi>
2023-11-01 16:04:34 +0200

    delete unnecessaray icon

--- src/page/About/About.tsx
+++ src/page/About/About.tsx
@@ -2,16 +2,13 @@ import { AboutObj } from "../../data/AboutData/Index";
 import AboutDetails from "../../components/AboutDetails/AboutDetails";
 import { MitionAnimate } from "../../components";
 
- 
-
 const About = () => {
   return (
     <section className='container mx-auto px-4 space-y-12 md:space-y-16 lg:space-y-20 py-10'>
-    <MitionAnimate>
-     <AboutDetails {...AboutObj}/>
-    </MitionAnimate>
-  </section>
-
+      <MitionAnimate>
+        <AboutDetails {...AboutObj} />
+      </MitionAnimate>
+    </section>
   );
 };
 
