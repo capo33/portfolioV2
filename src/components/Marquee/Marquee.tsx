37febb6f7f9108edabb8962d295fd6de2aba7e30
Mohamed <s2000588@edu.bc.fi>
2023-11-02 21:01:43 +0200

    update marquee

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -10,11 +10,15 @@ interface MarqueeProps {
 const Marquee = ({ link }: MarqueeProps) => {
   return (
     <Link to={link}>
-      <M pauseOnHover={true} speed={100} className='lg:pb-10'>
+      <M pauseOnHover={true} speed={100}>
         <div className='flex flex-wrap justify-center pt-10 lg:justify-between'>
           {Icons.map((icon) => (
-            <div className='lg:px-16' key={icon.id}>
-              <img src={icon.icon} alt='' className="w-12 hover:scale-125 transition-all duration-300 ease-in-out" />
+            <div className='lg:px-16 md:px-10 px-5 pb-10' key={icon.id}>
+              <img
+                src={icon.icon}
+                alt=''
+                className='w-16 pr-5 hover:scale-125 transition-all duration-300 ease-in-out'
+              />
             </div>
           ))}
         </div>
