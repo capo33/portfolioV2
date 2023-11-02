7468c8ad050904c32dc15b0c6dc13a16bc50e8de
Mohamed <s2000588@edu.bc.fi>
2023-11-02 13:34:58 +0200

    add SVG icon instead of windows icon to snippet

--- src/components/AboutDetails/AboutDetails.tsx
+++ src/components/AboutDetails/AboutDetails.tsx
@@ -1,5 +1,6 @@
 import { SharedSection } from "../../shared";
 import { IAboutMainObj } from "../../interfaces";
+import { SuccessSVG } from "../../assets/svg/About";
 
 const AboutDetails = ({
   firstHeadLine,
@@ -9,30 +10,28 @@ const AboutDetails = ({
   img,
 }: IAboutMainObj) => {
   return (
-    <>
-      <SharedSection
-        topLine={topLine}
-        firstHeadLine={firstHeadLine}
-        secondHeadLine={secondHeadLine}
-        img={img}
-        imgStart
-      >
-        <div className='text-gray-500 xl:text-lg md:text-base dark:text-gray-400'>
-          <ul className='space-y-2'>
-            {description &&
-              description.map((item) => (
-                <li
-                  className='flex space-x-3 lg:text-lg items-center p-2 hover:bg-gray-200'
-                  key={item.id}
-                >
-                  <span className='text-lg'>{item.emoji}</span>
-                  <span className='text-base text-gray-900 '>{item.title}</span>
-                </li>
-              ))}
-          </ul>
-        </div>
-      </SharedSection>
-    </>
+    <SharedSection
+      topLine={topLine}
+      firstHeadLine={firstHeadLine}
+      secondHeadLine={secondHeadLine}
+      img={img}
+      imgStart
+    >
+      <div className='text-gray-500 xl:text-lg md:text-base dark:text-gray-400'>
+        <ul className='space-y-2'>
+          {description &&
+            description.map((item) => (
+              <li
+                className='flex space-x-3 lg:text-lg items-center p-2 hover:bg-gray-200'
+                key={item.id}
+              >
+                <span className='text-lg'>{SuccessSVG}</span>
+                <span className='text-base text-gray-900 '>{item.title}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </SharedSection>
   );
 };
 
