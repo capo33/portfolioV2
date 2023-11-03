2f3ce1def2513d981a53ed889422f6add9ee32b0
Mohamed <s2000588@edu.bc.fi>
2023-11-03 10:43:50 +0200

    change footer style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
-import { GitHubSVG, LinkedInSVG } from "../../assets/svg/NavSVG";
+import { Link } from "react-router-dom";
+import { GitHubSVG, LinkedInSVG, SpiderSVG } from "../../assets/svg/NavSVG";
+import { AnchorTag } from "../../shared";
 
 const Footer = () => {
   return (
@@ -18,34 +20,27 @@ const Footer = () => {
       </svg>
       <div className='bg-gradient-to-b from-gray-100 to-transparent dark:from-gray-800 dark:to-transparent pt-1'>
         <div className='container m-auto text-gray-600 dark:text-gray-400 px-4'>
-          <div className='grid grid-cols-8 gap-6 md:gap-0'>
+          <div className='grid lg:grid-cols-8  md:grid-cols-8 gap-6 md:gap-0'>
             <div className='col-span-8 border-r border-gray-100 dark:border-gray-800 md:col-span-2 lg:col-span-3'>
               <div className='flex items-center justify-between gap-6 border-b border-white dark:border-gray-800 py-6 md:block md:space-y-6 md:border-none md:py-0'>
-                <div>
-                  <a
-                    href='#'
-                    className='text-xl font-semibold tracking-widest text-gray-800 uppercase rounded-lg dark:text-white hover:text-gray-700 dark:hover:text-gray-300'
-                  >
+                <Link to='/' className='flex items-center gap-2'>
+                  {SpiderSVG("h-6 w-6 text-blue-500 dark:text-white")}
+                  <span className='text-2xl font-semibold whitespace-nowrap'>
                     Mo
-                  </a>
-                </div>
+                  </span>
+                </Link>
+                <div></div>
                 <div className='flex gap-6'>
-                  <a
-                    href='#'
-                    target='blank'
-                    aria-label='github'
-                    className='hover:text-[#171515]'
-                  >
-                    {GitHubSVG("h-6 w-6")}
-                  </a>
-                  <a
-                    href='#'
-                    target='blank'
-                    aria-label='twitter'
-                    className='hover:text-[#0072b1]'
-                  >
-                    {LinkedInSVG("h-6 w-6")}
-                  </a>
+                  <AnchorTag
+                    href='https://github.com/capo33'
+                    icon={GitHubSVG("h-6 w-6")}
+                    aClass="hover:text-black"
+                  />
+                  <AnchorTag
+                    href='https://www.linkedin.com/in/mohamed-capo/'
+                    icon={LinkedInSVG("h-6 w-6")}
+                    aClass="hover:text-[#0A66C2]"
+                  />
                 </div>
               </div>
             </div>
@@ -100,8 +95,19 @@ const Footer = () => {
               </div>
               <div className='flex justify-between border-t border-gray-100 dark:border-gray-800 py-4 pb-8 '>
                 <span>
-                  © Copyright  Mo {new Date().getFullYear()} - All rights reserved
+                  © Copyright Mo {new Date().getFullYear()} - All rights
+                  reserved
                 </span>
+                {/* <div className='flex gap-2'>
+                      <AnchorTag
+                        href='https://github.com/capo33'
+                        icon={GitHubSVG("h-6 w-6")}
+                      />
+                      <AnchorTag
+                        href='https://www.linkedin.com/in/mohamed-capo/'
+                        icon={LinkedInSVG("h-6 w-6")}
+                      />
+                    </div> */}
               </div>
             </div>
           </div>
