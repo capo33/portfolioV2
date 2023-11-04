a6607b5ec36b3ae3945dbcba8866fdf8eb8fc472
Mohamed <s2000588@edu.bc.fi>
2023-11-04 09:59:05 +0200

    update contact form

--- src/components/ContactDetails/ContactDetails.tsx
+++ src/components/ContactDetails/ContactDetails.tsx
@@ -27,13 +27,13 @@ const ContactDetails = ({
             </div>
 
             <input
-              className='w-full px-3 py-2 mb-4 outline-none leading-loose border rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-700 dark:border-gray-700'
+              className='w-full px-3 py-2 mb-4 outline-none border rounded bg-gray-100'
               type='text'
               placeholder='Name'
               required
             />
             <input
-              className='w-full px-3 py-2 mb-4 outline-none leading-loose border rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-700 dark:border-gray-700'
+              className='w-full px-3 py-2 mb-4 outline-none border rounded bg-gray-100'
               type='email'
               placeholder='Email'
               required
@@ -43,7 +43,7 @@ const ContactDetails = ({
               typeof='message'
               placeholder='Write a message...'
               required
-              className='block w-full px-4 mb-4 outline-none leading-tight text-gray-700 border rounded bg-gray-50 dark:placeholder-gray-400 py-7 dark:text-gray-400 dark:border-gray-800 dark:bg-gray-700 '
+              className='block w-full px-4 mb-4 outline-none border rounded py-7 resize-none bg-gray-100'
               defaultValue={""}
             />
             <button className='w-full py-4 text-sm font-bold leading-normal text-white transition-all duration-300 bg-gray-800 rounded-md hover:bg-gray-950'>
