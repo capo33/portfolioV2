af72655193846036c1adbb6ab859c82169ac96f6
Mohamed <s2000588@edu.bc.fi>
2023-11-06 23:27:15 +0200

    add default class value to anchor tag component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-
 import { SharedSection, AnchorTag } from "../../shared";
 import IHomeMainObj from "../../interfaces/HomeInterface";
 
@@ -22,16 +21,8 @@ const Hero = ({
       img={img}
     >
       <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
-        <AnchorTag
-          icon={firstIcon}
-          label={firstLabel}
-          aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-        />
-        <AnchorTag
-          icon={secondIcon}
-          label={secondLabel}
-          aClass='inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
-        />
+        <AnchorTag icon={firstIcon} label={firstLabel} />
+        <AnchorTag icon={secondIcon} label={secondLabel} />
       </div>
     </SharedSection>
   );
--- src/shared/Anchor/Anchor.tsx
+++ src/shared/Anchor/Anchor.tsx
@@ -7,7 +7,7 @@ interface IAnchor {
   sClass?: string;
   href?: React.AnchorHTMLAttributes<HTMLAnchorElement>["href"];
 }
-const Anchor = ({ icon, label, aClass, sClass, href }: IAnchor) => {
+const Anchor = ({ icon, label, aClass="inline-flex items-center cursor-pointer justify-center w-full px-5 py-3 text-sm font-medium text-center border border-gray-200 rounded sm:w-auto bg-gray-50 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800", sClass, href }: IAnchor) => {
   return (
     <a href={href} className={aClass} target='_blank' rel='noreferrer'>
       <span className={sClass}>{icon}</span>
