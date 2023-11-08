d614fc35db0f637877f51d804b493ba1751e88a3
Mohamed <s2000588@edu.bc.fi>
2023-11-08 10:49:23 +0200

    update theme context

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,17 +1,26 @@
-import { createContext } from "react";
-
-import { theme } from "../theme";
+import { createContext, useState } from "react";
 
 type ThemeContextProviderProps = {
   children: React.ReactNode;
 };
 
-export const ThemeContext = createContext(theme);
+type ThemeContextProps = {
+  toggle: boolean;
+  setToggle: (toggle: boolean) => void;
+};
+
+export const ThemeContext = createContext<ThemeContextProps>({
+  toggle: false,
+  setToggle: () => {},
+});
 
 export const ThemeContextProvider = ({
   children,
 }: ThemeContextProviderProps) => {
+  const [toggle, setToggle] = useState<boolean>(false);
   return (
-    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ toggle, setToggle }}>
+      {children}
+    </ThemeContext.Provider>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
 import "./index.css";
+import { ThemeContextProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <ThemeContextProvider>
+      <RouterProvider router={routes} />
+    </ThemeContextProvider>
   </React.StrictMode>
 );
