e7ddd7e2bc6cd82391e2cd58425655daa34a0efb
Mohamed <s2000588@edu.bc.fi>
2023-11-10 19:28:09 +0200

    update TechCard bg dark mode

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -8,18 +8,34 @@ import {
   StylesIcons,
   ToolsIcons,
 } from "../../data/AboutData";
+import { HeaderTitle } from "../../shared";
 
 const Technologies = () => {
   return (
-    <section className='container mx-auto py-10'>
-      <span className='font-semibold text-lg'>
-        Technologies
-      </span>
-      <span className='ml-2 inline-block h-[1px] w-8 bg-blue-400'></span>
-      <h2 className='max-w-5xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white'>
-        Tech
-        <span className='text-blue-500'> Stack</span>
-      </h2>
+    // <section className='container mx-auto py-10'>
+    //   <span className='font-semibold text-lg'>
+    //     Technologies
+    //   </span>
+    //   <span className='ml-2 inline-block h-[1px] w-8 bg-blue-400'></span>
+    //   <h2 className='max-w-5xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white'>
+    //     Tech
+    //     <span className='text-blue-500'> Stack</span>
+    //   </h2>
+    // <div className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-10 py-5'>
+    //   <TechCard data={FrontEndIcons} title='Frontend' />
+    //   <TechCard data={StylesIcons} title='Styles' />
+    //   <TechCard data={BackEndIcons} title='Backend' />
+    //   <TechCard data={DatabaseIcons} title='Database & API' />
+    //   <TechCard data={ToolsIcons} title='Tools' />
+    //   <TechCard data={GameIcons} title='Game' />
+    //   <TechCard data={CloudIcon} title='Cloud' />
+    // </div>
+    // </section>
+    <HeaderTitle
+      topLine='Technologies'
+      firstHeadLine='Tech'
+      secondHeadLine=' Stack'
+    >
       <div className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-10 py-5'>
         <TechCard data={FrontEndIcons} title='Frontend' />
         <TechCard data={StylesIcons} title='Styles' />
@@ -29,7 +45,7 @@ const Technologies = () => {
         <TechCard data={GameIcons} title='Game' />
         <TechCard data={CloudIcon} title='Cloud' />
       </div>
-    </section>
+    </HeaderTitle>
   );
 };
 
--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -9,46 +9,6 @@ interface ITimeLineProps {
 
 const TimeLine = ({ educaction, workExperience }: ITimeLineProps) => {
   return (
-    // <section className='container mx-auto py-10'>
-    //   <span className='font-semibold text-lg'>
-    //     Experience & Education
-    //   </span>
-    //   <span className='ml-2 inline-block h-[1px] w-8 bg-cyan-400'></span>
-    //   <h2 className='max-w-5xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white'>
-    //     My
-    //     <span className='text-cyan-500'> Qualification</span>
-    //   </h2>
-    //   <div className='grid lg:grid-cols-2'>
-    //     <div>
-    //       <h3 className='text-2xl font-bold flex py-5'>💼 Experience</h3>
-    //       {workExperience.map((item) => {
-    //         return (
-    //           <TimeLineData
-    //             key={item.id}
-    //             time={item.date}
-    //             what={item.title}
-    //             where={item.company}
-    //             desc={item.description}
-    //           />
-    //         );
-    //       })}
-    //     </div>
-    //     <div>
-    //       <h3 className='text-2xl font-bold flex py-5'>📚 Education</h3>
-    //       {educaction.map((item) => {
-    //         return (
-    //           <TimeLineData
-    //             key={item.id}
-    //             time={item.date}
-    //             what={item.title}
-    //             where={item.company}
-    //             desc={item.description}
-    //           />
-    //         );
-    //       })}
-    //     </div>
-    //   </div>
-    // </section>
     <HeaderTitle
       topLine='Experience & Education'
       firstHeadLine='My'
