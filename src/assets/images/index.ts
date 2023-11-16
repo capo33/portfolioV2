844924567c17176fe4750ab6247ab49369e462a7
Mohamed <s2000588@edu.bc.fi>
2023-11-16 22:19:19 +0200

    delete unnecessary image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,4 @@
-import Coding from "./coding.gif";
 import Coding2 from "./coding2.gif";
-import Rocket from "./rocket.png";
 import Rocket2 from "./rocket2.png";
 
-export { Coding,Coding2, Rocket,Rocket2 };
+export { Coding2, Rocket2 };
