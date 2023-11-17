a3a9bb6b6878cbbdc7afb384484102d1b58965e5
Mohamed <s2000588@edu.bc.fi>
2023-11-17 08:58:32 +0200

    add resolve & build objects to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,18 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   define: {
+  resolve: {
+    alias: [
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+    ],
+  },
+  build: {
+    outDir: "build",
+  },
+  define: {
     "process.env": {
       SERVICE_ID: JSON.stringify(process.env.SERVICE_ID),
       TEMPLATE_ID: JSON.stringify(process.env.TEMPLATE_ID),
