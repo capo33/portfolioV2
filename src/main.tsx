310dc4d93d5fc4e4c20b61aa26d1f9eaff2ae310
Mohamed <s2000588@edu.bc.fi>
2023-11-20 02:40:53 +0200

    change aws-exports file directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
 
 import routes from "./routes/routes";
 
