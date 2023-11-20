066149ccbe19bae0c30eab85ec0b1c81c0166980
Mohamed <s2000588@edu.bc.fi>
2023-11-20 02:50:19 +0200

    remove aws-amplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { Amplify } from "aws-amplify";
-import awsExports from "../aws-exports";
-
+ 
 import routes from "./routes/routes";
 
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext";
 
-Amplify.configure(awsExports);
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
       <RouterProvider router={routes} />
