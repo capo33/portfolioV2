2ec4c46848c2b958ece1c2386ea369ba75f40132
Mohamed <s2000588@edu.bc.fi>
2023-11-21 06:47:27 +0200

    install aws-amplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import routes from "./routes/routes";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
- 
-import routes from "./routes/routes";
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext";
+
+Amplify.configure(awsExports);
+
 
  ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
